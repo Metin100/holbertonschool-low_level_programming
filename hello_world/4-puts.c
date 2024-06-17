75ba8eaa4e513590c698afb71219e440bb2f5f4a
Metin100 <9455@holbertonstudents.com>
2024-06-17 12:06:08 +0000

    fhsd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/*
+ * Function: main
+ * ----------------------------
+ * Entry point of the program. Prints a message using the puts function.
+ *
+ * Returns: 0 on successful execution.
+ */
 int main(void)
 {
 const char *message = "\"Programming is like building a multilingual puzzle";
