f2626fa8be305a53dd44be36309c2e0862f0451b
Metin100 <9455@holbertonstudents.com>
2024-06-17 12:02:22 +0000

    fhsd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-const char *message = "Programming is like building a multilingual puzzle";
+const char *message = "\"Programming is like building a multilingual puzzle";
 puts(message);
 return (0);
 }
