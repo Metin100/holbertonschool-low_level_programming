ff6240e23a34ff19f101b70aa46c7713fd5b2539
Metin100 <9455@holbertonstudents.com>
2024-06-17 12:03:13 +0000

    fhsd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Description: Printing "\"Programming is like building a multilingual puzzle";
- * @parameter: void
- *
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
 const char *message = "\"Programming is like building a multilingual puzzle";
