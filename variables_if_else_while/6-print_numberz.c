7784b9cdfeca457858182ef56df4e5c4adc652c7
Metin100 <9455@holbertonstudents.com>
2024-06-19 15:35:50 +0000

    dfekcvbw

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int c;
+	for (c = '1'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
