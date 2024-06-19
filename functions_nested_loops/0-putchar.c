54581df71f77565cf65419fd48fdfc1164bb1b95
Metin100 <9455@holbertonstudents.com>
2024-06-19 16:25:57 +0000

    dksa

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,6 +8,14 @@
 int main(void)
 {
 
-	printf("_putchar");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
