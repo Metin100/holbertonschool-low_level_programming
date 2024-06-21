19f1bfcad44ff0376f9c5caba36a52c488aa1b87
Metin100 <9455@holbertonstudents.com>
2024-06-21 14:12:48 +0000

    dksa

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,24 +1,30 @@
 #include "main.h"
 
+/**
+ * times_table - Prints the 9 times table, starting with 0
+ *
+ */
 void times_table(void)
 {
-	int mult, dmp, count;
-	for (count = 0; count <= 9; count++)
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-		for (mult = 1;mult <= 9; mult++)
+
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
-			
-			dmp = mult * count;
 
-			if (dmp <= 9)
+			prod = num * mult;
+
+			if (prod <= 9)
 				_putchar(' ');
-			else 
-				_putchar((dmp / 10) + '0');
+			else
+				_putchar((prod / 10) + '0');
 
-			_putchar((dmp % 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
