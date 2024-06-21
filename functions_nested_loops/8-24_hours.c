6317a001cad70c012e6a557559540e544e71e96f
Metin100 <9455@holbertonstudents.com>
2024-06-21 13:37:24 +0000

    dksa

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/**
+ * jack_bauer - Prints every minute of the day of
+ * Jack Bauer, starting from 00:00 to 23:59
+ */
 void jack_bauer(void)
 {
- 	int minute , hours;
-	for(hours = 0; hours <= 23; hours++)
+	int minute, hours;
+
+	for (hours = 0; hours <= 23; hours++)
 	{
-		for(minute = 0; minute < 60 ; minute++)
+		for (minute = 0; minute < 60 ; minute++)
 		{
 			_putchar(hours / 10 + '0');
 			_putchar(hours % 10 + '0');
