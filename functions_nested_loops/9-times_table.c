c5bbeab1f174c7d3f0c2542290179d22293ecb55
Metin100 <9455@holbertonstudents.com>
2024-06-21 13:53:05 +0000

    dksa

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,7 +8,10 @@ void times_table(void)
 		for(c = 0; c <= 9; c++)
 		{
 			int k = a * c;
-			_putchar(k + '0');
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
 		}
 		_putchar('\n');
 	}
