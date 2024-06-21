b066669612adcc48bd6a2c8426bde5c53e15a922
Metin100 <9455@holbertonstudents.com>
2024-06-21 13:02:48 +0000

    dksa

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *  _abs - computes the absolute value of an integer
+ *  @n: the int to check
+ *
+ *  Return: the absolute value of int
+ */
+int _abs(int n){
+	return (n >= 0 ? n : -n);
+}
