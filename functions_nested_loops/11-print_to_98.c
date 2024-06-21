c0e79eb25c67d4c551cb0eb2af44578e63d707f3
Metin100 <9455@holbertonstudents.com>
2024-06-21 14:21:59 +0000

    dksa

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -4,14 +4,18 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		while n >= 98
+		while (n >= 98)
+		{
 		printf("%d, ",n);
 		n--;
+		}
 	}
 	else if (n <= 98)
 	{
-		while n <= 98
+		while (n <= 98)
+		{
 			printf("%d, ",n);
 			n++;
+		}
 	}
 }
