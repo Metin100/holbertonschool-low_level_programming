42293aa22e210f90f84b30d3c0204b4fa011e498
Metin100 <9455@holbertonstudents.com>
2024-06-21 14:20:56 +0000

    dksa

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while n >= 98
+		printf("%d, ",n);
+		n--;
+	}
+	else if (n <= 98)
+	{
+		while n <= 98
+			printf("%d, ",n);
+			n++;
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -15,3 +15,4 @@ int _putchar(char c);
 	void jack_bauer(void);
 	void times_table(void);
 	int add(int, int);
+	void print_to_98(int n);
