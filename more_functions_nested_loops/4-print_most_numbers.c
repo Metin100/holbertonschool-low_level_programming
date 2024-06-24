8a70b7b424810fa9203db334f2003ff4db5974b4
Metin100 <9455@holbertonstudents.com>
2024-06-24 08:19:51 +0000

    dertge

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
+/**
+ * print_most_numbers - prints numbers except 2 and
+ * Return: returns nothing
+ */
+
 void print_most_numbers(void)
 {
 	int k;
 
-	for(k = 0;k < 10; k++)
+	for (k = 0; k < 10; k++)
 	{
-		if(k != 2 && k != 4)
+		if (k != 2 && k != 4)
 		{
 			_putchar(k + '0');
 		}
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
