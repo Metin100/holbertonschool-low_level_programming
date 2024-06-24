b03c91e64745c19eace99d7173c4cc5b44ef09b8
Metin100 <9455@holbertonstudents.com>
2024-06-24 08:24:32 +0000

    dertge

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "math.h"
+
+void more_numbers(void)
+{
+	int k , i;
+
+	for (k = 0; k < 10; k++)
+	{
+		for(i = 0; i < 15; i++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
