9145147759350aec9ce9ba35d86c0a288ef29567
Metin100 <9455@holbertonstudents.com>
2024-06-24 09:39:50 +0000

    dertge

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		
+		else if ( i % 3 == 0)
+			printf("Fizz");
+		
+		else if ( i % 5 == 0)
+			printf("Buzz");
+
+		else
+			printf("%d",i);
+	}
+	printf('\n');
+}
