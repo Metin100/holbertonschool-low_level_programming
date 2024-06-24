d9d4fa1f34813236a151070f0aa15827ccec7364
Metin100 <9455@holbertonstudents.com>
2024-06-24 07:37:43 +0000

    dertge

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+int _isupper (int c)
+{
+	char k = 'c + '0'';
+
+	if (c >= "A" && c <= "Z")
+	{
+		return 1;
+	}
+	else if (c >= "a" && c <= "z")
+	{
+		return 0;
+	}
+}
