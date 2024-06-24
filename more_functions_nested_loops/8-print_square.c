c30e3362c2d4c2917d35db694203e88b85068ded
Metin100 <9455@holbertonstudents.com>
2024-06-24 09:22:14 +0000

    dertge

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,13 +2,13 @@
 
 void print_square(int size)
 {
-	int k,j;
+	int k, j;
 
 	if (size > 0)
 	{
 		for (j = 0; j < size; j++)
 		{
-			for (k = 0; k < size-1; k++)
+			for (k = 0; k < size - 1; k++)
 			{
 				_putchar('#');
 			}
