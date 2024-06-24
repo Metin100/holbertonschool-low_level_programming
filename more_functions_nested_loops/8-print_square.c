661c93bf0201b2a5b0cb2c320a42bac6de4155ba
Metin100 <9455@holbertonstudents.com>
2024-06-24 09:23:08 +0000

    dertge

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,11 +8,10 @@ void print_square(int size)
 	{
 		for (j = 0; j < size; j++)
 		{
-			for (k = 0; k < size - 1; k++)
+			for (k = 0; k < size; k++)
 			{
 				_putchar('#');
 			}
-			_putchar('#');
 			_putchar('\n');
 		}
 	}
