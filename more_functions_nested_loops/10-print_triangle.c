73cca21229f5519c84232b4a95c45a4789c87975
Metin100 <9455@holbertonstudents.com>
2024-06-24 15:20:41 +0000

    dertge

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "math.h"
+
+void print_triangle(int size)
+{
+	int setir,sutun;
+	if (size > 0)
+	{
+		for (setir = 1; setir <= size; setir--)
+		{
+			for (sutun = 1; sutun <= size - setir; sutun ++)
+			{
+				_putchar(' ');
+			}
+			for (; sutun <= size ; sutun++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
