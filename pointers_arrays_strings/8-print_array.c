fb10b3bdd8698f3af51972f0ee9b4a874e5e7364
Metin100 <9455@holbertonstudents.com>
2024-06-26 13:50:21 +0000

    Salam

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -5,10 +5,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ",a[i]);
+		printf("%d",a[i]);
+
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
 	}
-	printf("%d",a[i]);
 	printf("\n");
 }
