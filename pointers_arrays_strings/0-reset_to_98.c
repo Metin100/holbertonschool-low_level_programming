a14f53be649427f65e2622d251f3db543389b411
Metin100 <9455@holbertonstudents.com>
2024-06-26 11:05:08 +0000

    Salam

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+*reset_to_98 - function that takes a pointer to an int as parametr and
+*updates the value it points to the 98.
+*
+*@n: This is the entry
+*/
+
 void reset_to_98(int *n)
 {
 	*n = 98;
--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+* swap_int- function that swaps the values of two integers
+* @a: this is the first entry
+* @b: this is the second entry
+*/
 void swap_int(int *a, int *b)
 {
 	int c = *a;
