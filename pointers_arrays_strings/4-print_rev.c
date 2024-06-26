155ef0682368a4509fab259da2b63c88eba18188
Metin100 <9455@holbertonstudents.com>
2024-06-26 10:51:04 +0000

    Salam

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
+/**
+* print_rev - function that prints a string, followed by a new line, to stdout
+*
+* @s: This is my input String
+*/
+
 void print_rev(char *s)
 {
-	int index;
+	int i;
 
-	for(index = sizeof(s) + 1; index >= 0; index--)
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	for (i = i - 1; s[i] != '\0'; i--)
 	{
-		_putchar(s[index]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
