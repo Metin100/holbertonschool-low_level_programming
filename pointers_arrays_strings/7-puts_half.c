edb4016e6808941bd445f72f2eee73a4f2bf721d
Metin100 <9455@holbertonstudents.com>
2024-06-26 13:27:42 +0000

    Salam

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int length, i;
+
+	for (length = 0; str[length] != '\0' && str[length + 1] != '\0'; length++)
+	{
+		continue;
+	}
+	
+	length = length + 1;
+
+	if (length % 2 == 0)
+	{
+		for (i = length / 2; i <= length; i++;)
+		{
+			_putchar(*str[i]);
+		}
+	}
+	else
+	{
+		for (i = (length - 1) / 2; i <= length; i++;)
+                {
+                        _putchar(*str[i]);
+                }
+	}
+	_putchar('\n');
+}
