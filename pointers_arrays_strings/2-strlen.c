51d34010987b5f3273e8c012957eecf0a947b8a2
Metin100 <9455@holbertonstudents.com>
2024-06-26 11:05:53 +0000

    Salam

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <string.h>
 
+/**
+* _strlen - Function that returns the length of a string
+*@s: this is the input string
+*Return: Length of the string
+*/
 int _strlen(char *s)
 {
 	int index;
