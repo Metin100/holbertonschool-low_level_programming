16d926dde420279c983ec702be42bda35fd273ce
Metin100 <9455@holbertonstudents.com>
2024-06-28 11:33:42 +0000

    Salam

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,25 @@
 #include "main.h"
 
+/**
+*_strcat - concatenates  the string pointed to by @src to
+*the end of the string pointed to by @dest
+*@dest: String that will be appended
+*@src: String to be concatenated upon
+*
+*Return: returns poiner to @dest
+*/
+
+
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, length = 0;
+	int i = 0;
+	int len = 0;
 
 	while (dest[i++])
-		length++;
+		len++;
 
 	for (i = 0; src[i]; i++)
-	{
-		dest[length++] = src[i];
-	}
+		dest[len++] = src[i];
 
 	return (dest);
 }
