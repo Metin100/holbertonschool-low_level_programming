05c78e60ef0a21266734608d751c90746d5640d6
Metin100 <9455@holbertonstudents.com>
2024-06-28 12:02:58 +0000

    Salam

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,5 +14,6 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
