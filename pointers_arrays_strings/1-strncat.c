9ebc27f69c77b61cb56630527da92ff6f9cb4a77
Metin100 <9455@holbertonstudents.com>
2024-06-28 11:42:54 +0000

    Salam

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,5 +2,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	
+	int i = 0; len = 0;
+
+	while (dest[i++])
+		len++;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[len++] = src[i];
+	}
+	return (dest);
 }
