c98217dca17c7caa290943ec8291beac1097c7f8
Metin100 <9455@holbertonstudents.com>
2024-06-28 12:33:09 +0000

    Salam

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,11 +2,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, res = 0;
+	for (; (*s1 && *s2) && (*s1 == *s2); s1++, s2++)
+		;
 
-	for (i = 0; s1[i] && s2[i]; i++)
+	if (*s1 == *s2)
 	{
-		res += (s1[i] - s2[i]);
+		return (0);
 	}
-	return (res);
+
+	return (*s1 - *s2);
 }
