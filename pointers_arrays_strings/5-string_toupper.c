0c806618d4f0a6a2e1bed77a7542db60f08e33f3
Metin100 <9455@holbertonstudents.com>
2024-06-28 13:24:57 +0000

    Salam

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+*string_toupper - changes all lowercase letters to uppercase
+*@str: string to be changed
+*
+*Return: address to the string
+*/
+
 char *string_toupper(char *str)
 {
 	int i = 0;
