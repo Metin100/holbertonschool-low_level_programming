412591bb3e769cf82e81169e08a00df5d7a172a3
Metin100 <9455@holbertonstudents.com>
2024-06-29 11:36:18 +0000

    Salam

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,6 +9,10 @@
 * Return: if the strings are equals return "0", if not return other number
 */
 
+//we are searching different symbol between two arrays
+//in the condition of loop (we are looking for null condition for both arrays) && (we are checking if symbol's are different or not)
+//if both array is equal , we have to return value 0f 0
+//if it isn't we have to find first different symbol and we have to exit their ascii codes
 
 int _strcmp(char *s1, char *s2)
 {
