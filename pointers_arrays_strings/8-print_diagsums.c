a78faf99db86bb8b31bf1064506d207f5902a57f
Metin100 <9455@holbertonstudents.com>
2024-07-06 12:25:05 +0000

    Salam

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+void print_diagsums(int *a, int size)
+{
+	int i = 0, j = 0, sum = 0;
+	for (; i < size; i++)
+	{
+		for (; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum = sum + a[i][j];
+			}
+		}
+	}
+
+	printf ("%d ",sum);
+	sum = 0;
+	for (i = i - 1; i >= 0; i--)
+	{
+		for (j = j - 1; j >= 0; j--)
+		{	
+			if (i == j)
+			{
+				sum = sum + a[i][j];
+			}
+		}
+	}
+	printf ("%d ",sum);
+
+}
