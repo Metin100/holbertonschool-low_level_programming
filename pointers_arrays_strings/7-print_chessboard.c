b4e4fa5c6c3d22774aa09be50d64c1d03f1cc8e3
Metin100 <9455@holbertonstudents.com>
2024-07-06 12:11:11 +0000

    Salam

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+/**
+*print_chessboard - print chessboard given set 2D array
+*@a: 2D array
+*/
+
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i< 8;i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j=0; j< 8; j++)
+		for (j = 0; j < 8; j++)
 		{
-			printf("%c",a[i][j]);
+			printf("%c", a[i][j]);
 		}
 		printf("\n");
 	}
