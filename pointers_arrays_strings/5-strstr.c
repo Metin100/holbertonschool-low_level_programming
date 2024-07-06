9b71d8e2b53d1b413f9d0f12f9ab5d184a9e537c
Metin100 <9455@holbertonstudents.com>
2024-07-06 12:00:40 +0000

    Salam

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			while (needle)
+			{
+				if (*haystack != *needle)
+				{
+					break;
+				}
+				needle++;
+			}
+			if (*needle == '\0')
+			{
+				return (haystack);
+			}
+		}
+		haystack++;
+	}
+}
