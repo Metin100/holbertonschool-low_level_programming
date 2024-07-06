f6d0f09748c51d6a994cea70407232ccc31883cc
Metin100 <9455@holbertonstudents.com>
2024-07-06 11:14:17 +0000

    Salam

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strchr - Returns a pointer to the first occurrence
+*of the character c in the string s, or NULL if the
+*character is not found
+*
+*@s:string targeted
+*@c:character targeted
+*
+*Return: returns pointer to first occcurence of c
+*/
+
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
@@ -8,7 +22,7 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	if (*s == '\0')
+	if (c == '\0')
 	{
 		return (s);
 	}
