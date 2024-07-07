031721a856d72dfc368585ced9b17f8185f2748f
Metin100 <9455@holbertonstudents.com>
2024-07-07 11:23:23 +0000

    adsfbs

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+*_pow_recursion - function that returns the
+*value of x raised to the power of y
+*@x: the base
+*@y: the exponent
+*Return: the value of x raised to the power of y
+*/
+
 int _pow_recursion(int x, int y)
 {
-	int i = 0;
-
-	if (i <= y)
-	{
-		i += 1;
-		return x * _pow_recursion(x)
-	}
+		if (y < 0)
+			return (-1);
+		else if (y == 0)
+			return (1);
+		return (_pow_recursion(x, y - 1) * x);
 }
