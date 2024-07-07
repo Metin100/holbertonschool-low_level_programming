1e208489f928d4a9fba7043d8cd7176667368bf7
Metin100 <9455@holbertonstudents.com>
2024-07-07 12:42:33 +0000

    dfsghds

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc , char *argv[])
+{
+	int sum = 1, i;
+
+	if (argc <= 1)
+	{
+		printf("Error");
+	}
+	else
+	{
+		for (i = 1; argv[i]; i++)
+		{
+			sum = sum * atoi(argv[i]);
+		}
+	}
+
+	printf("%d\n",sum);
+}
