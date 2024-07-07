25b46c1de5c3d57624043b464792498856b6a46b
Metin100 <9455@holbertonstudents.com>
2024-07-07 11:09:11 +0000

    adsfbs

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,13 +1,22 @@
 #include "main.h"
 
+/**
+*factorial - search for the factorial of a number
+*@n: the number to find the factorial of
+*
+*Return: the factorial of the number
+*/
+
 int factorial(int n)
 {
+	int next;
+
 	if (n < 0)
 		return (-1);
 
-	else if (n == 1)
+	else if (n == 0)
 		return (1);
 
-	else
-		return n * (factorial(n - 1));
+	next = factorial(n - 1);
+	return (next * n)
 }
