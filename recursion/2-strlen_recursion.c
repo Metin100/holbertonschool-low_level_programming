b42f9f28c685eca2c2d4d8de2adfa32247fa2719
Metin100 <9455@holbertonstudents.com>
2024-07-07 11:02:03 +0000

    adsfbs

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,12 +4,11 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s == '\0')
-		return i;
-
-	else
+	if (*s != '\0')
 	{
-		i += 1;
+		i++;
 		return (_strlen_recursion(s + 1));
 	}
+
+	return (i);
 }
