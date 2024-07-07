2a4ec51fc19fb7673341e6c137f9c2c363a76120
Metin100 <9455@holbertonstudents.com>
2024-07-07 11:31:56 +0000

    adsfbs

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -12,11 +12,11 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
 		return (n);
-	return (_findsqrt(0, n));
+	return (_sqrt(0, n));
 }
 
 /**
-*_findsqrt - returns the square root of a number
+*_sqrt - returns the square root of a number
 *@n: test number
 *@x: squared number
 *
@@ -24,7 +24,7 @@ int _sqrt_recursion(int n)
 *Return: the square root of n
 */
 
-int _findsqrt(int n, int x)
+int _sqrt(int n, int x)
 {
 	if (n > x / 2)
 		return (-1);
