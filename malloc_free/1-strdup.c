b278db8c20fab6e23767a6e25181d8ac79011182
Metin100 <9455@holbertonstudents.com>
2024-07-09 10:59:10 +0000

    htgrew

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char *_strdup(char *str)
+{
+	char *array;
+
+	if (str == NULL)
+		return (NULL);
+
+	int i;
+
+	for (i = 0; str[i]; i++)
+			;
+
+	k = malloc(sizeof(char) * i);
+
+	if (k == NULL)
+		return (NULL);
+
+	for (i = 0; k[i]; i++)
+	{
+		*k[i] = str[i];
+	}
+
+	return (k);
+}
