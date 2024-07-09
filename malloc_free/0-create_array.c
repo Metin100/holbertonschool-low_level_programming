a2c47a57b35b5634cd18ab0353a65f5c9f8b08a6
Metin100 <9455@holbertonstudents.com>
2024-07-09 10:45:23 +0000

    htgrew

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,21 +4,21 @@
 char *create_array(unsigned int size, char c)
 {
 	char *k;
-	k = malloc(sizeof(char)*size);
+        unsigned int i;
 
 	if (size == 0)
-	{
 		return NULL;
-	}
-	else if (size > 0)
-	{
-		unsigned int i;
 
-		for (i = 0; i < size; i++)
-		{
-			k[i] = c;
-		}
-		return k;
+	k = malloc(sizeof(char)*size);
+
+	if (k == NULL)
+		return (NULL);
+
+
+	for (i = 0; i < size; i++)
+	{
+		k[i] = c;
 	}
-	return NULL;
+		return (k);
+	
 }
