d5c2d23016e07da7d3bd66a0f259955f76cb82f4
Metin100 <9455@holbertonstudents.com>
2024-07-09 11:34:57 +0000

    htgrew

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char *str_concat(char *s1, char *s2)
+{
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	char *k;
+	int start = 0, end1, end2, end;
+
+	while (s1[start])
+		start++;
+
+	end1 = start;
+	start = 0;
+
+	while (s2[start])
+		start++;
+
+	end2 = start;
+
+	end = end1 + end2;
+
+	k = malloc(sizeof(char) * end);
+
+	if (k == NULL)
+		return (NULL);
+
+	start = 0;
+
+	while (start <= end)
+	{
+		if (start <= end1)
+		{
+			k[start] = s1[start];
+		}
+		else if (start >= end1)
+		{
+			k[start] = s2[start];
+		}
+	}
+	return (k);
+}
