399623a4c788f6e952aab8cb518a80870bc4e6ba
Metin100 <9455@holbertonstudents.com>
2024-07-11 09:13:23 +0000

    hgfds

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+*_calloc - allocates memory for an array.
+*@nmemb: number of elements.
+*@size: size of bytes.
+*
+*Return: pointer to the allocated memory.
+*if nmemb or size is 0, returns NULL.
+*if malloc fails, returns NULL
+*/
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *memory;
-	unsigned i;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,5 +31,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 
 	return (memory);
-	
+
 }
