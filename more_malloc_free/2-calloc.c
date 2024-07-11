45d53eddc5ed36f7d0c407379db440387518f142
Metin100 <9455@holbertonstudents.com>
2024-07-11 09:07:45 +0000

    hgfds

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *memory;
+	unsigned i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	memory = malloc(nmemb * size);
+
+	if (!memory)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		memory[i] = 0;
+	}
+
+	memory[i] = '\0';
+
+	return (memory);
+	
+}
