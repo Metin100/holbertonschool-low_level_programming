ab217e540c92e299dbd6c0488329c247f41ebde5
Metin100 <9455@holbertonstudents.com>
2024-07-11 09:24:42 +0000

    hgfds

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -4,5 +4,28 @@
 
 int *array_range(int min, int max)
 {
+	if (min > max)
+		return (NULL);
+
+	int i = 0, *memory;
+
+	while (min <= max)
+	{
+		min++;
+		i++;
+	}
 	
+	memory = malloc(sizeof(int)* i);
+
+	if (!memory)
+		return (NULL);
+
+	for (i = 0; min <= max; i++)
+	{
+		memory[i] = min;
+		min++;
+	}
+
+	return (memory);
+
 }
