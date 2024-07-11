a15f8f08e53a0a27692cd2ffa571adae7edd5ca2
Metin100 <9455@holbertonstudents.com>
2024-07-11 09:29:41 +0000

    hgfds

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* *array_range - make  memory for an array
+*@min: int type
+*@max: int type
+*
+*Return: return pointer to array
+*/
+
 int *array_range(int min, int max)
 {
 	int *memory, i = 0;
