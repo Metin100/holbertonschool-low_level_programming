cedb3d9eefbe01c50d7a74bf9a9eb5df58a2892e
Metin100 <9455@holbertonstudents.com>
2024-07-14 10:35:58 +0000

    Hello

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,19 +9,19 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("%s\n",d->name);
 		else
-			pritnf("(nil)\n");
+			printf("(nil)\n");
 
 		printf("Age: ");
                 if (d->age)
                         printf("%f\n",d->age);
                 else
-                        pritnf("(nil)\n");
+                        printf("(nil)\n");
 
 		printf("Owner: ");
                 if (d->name)
                         printf("%s\n",d->owner);
                 else
-                        pritnf("(nil)\n");
+                        printf("(nil)\n");
 
 	}
 }
