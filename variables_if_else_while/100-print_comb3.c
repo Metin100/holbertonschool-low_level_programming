f6a202764fabbf1af730814f698d7375c007830d
Metin100 <9455@holbertonstudents.com>
2024-07-14 12:02:29 +0000

    dfekcvbw

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a[10] = {0,1,2,3,4,5,6,7,8,9};
+    int b[10] = {0,1,2,3,4,5,6,7,8,9};
+    
+    int i,j;
+    for (i = 0; i < sizeof(a) / sizeof(int); i++)
+    {
+        
+        for (j = i; j < sizeof(b) / sizeof(int); j++)
+        {
+            if (a[i] != b[j])
+            printf("%d%d, ",a[i],b[j]);
+        }
+    }
+    return 0;
+}
