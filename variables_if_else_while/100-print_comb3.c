58e47687685183ee142454c17c6bee4eefd86fe8
Metin100 <9455@holbertonstudents.com>
2024-07-14 12:23:28 +0000

    dfekcvbw

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-	int i, j;
+	char i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = i + 1; j <= '9'; j++)
 			{
 					putchar(i);
 					putchar(j);
-					if (i < 8)
+					if (i < '8')
 					{
 						putchar(',');
 						putchar(' ');
