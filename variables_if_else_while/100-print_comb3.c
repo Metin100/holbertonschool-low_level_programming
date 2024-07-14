0b5ece48e459a1edac2e3e86b62c31291fb4124d
Metin100 <9455@holbertonstudents.com>
2024-07-14 12:12:37 +0000

    dfekcvbw

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int a[10] = {0,1,2,3,4,5,6,7,8,9};
-    int b[10] = {0,1,2,3,4,5,6,7,8,9};
+    char a[10] = {"0123456789"};
+    char b[10] = {"0123456789"};
     
     long unsigned int i,j;
-    for (i = 0; i < sizeof(a) / sizeof(int); i++)
+    for (i = 0; i < sizeof(a); i++)
     {
         
-        for (j = i; j < sizeof(b) / sizeof(int); j++)
+        for (j = i; j < sizeof(b); j++)
         {
+
             if (a[i] != b[j])
-            printf("%d%d, ",a[i],b[j]);
-        }
+	    {
+		putchar(a[i]);
+		putchar(b[j]);
+		if (i < 8)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	    }
+	   
+	}
     }
+    putchar('\n');
     return 0;
 }
