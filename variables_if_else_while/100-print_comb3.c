98320d4de4312a05e325a76014700680e0f3f5c6
Metin100 <9455@holbertonstudents.com>
2024-07-14 12:22:35 +0000

    dfekcvbw

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,14 @@
 
 int main(void)
 {
-	char a[10] = {"0123456789"};
-	char b[10] = {"0123456789"};
-	unsigned long int i, j;
+	int i, j;
 
-	for (i = 0; i < sizeof(a); i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = i + 1; j < sizeof(b); j++)
+		for (j = i + 1; j <= 9; j++)
 			{
-					putchar(a[i]);
-					putchar(b[j]);
+					putchar(i);
+					putchar(j);
 					if (i < 8)
 					{
 						putchar(',');
