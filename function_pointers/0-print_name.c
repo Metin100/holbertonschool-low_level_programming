48430d02c2d5230a8c68af733d1a2cae8061248f
Metin100 <9455@holbertonstudents.com>
2024-07-15 12:09:37 +0000

    sdgvrfsd

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+*print_name - Prints a name
+*@name: The name to prints
+*@f: Pointer to function
+*
+*Return: Nothing
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
-	if ((!name) && (!f))
+	if (name != NULL && f != NULL)
 		f(name);
 }
