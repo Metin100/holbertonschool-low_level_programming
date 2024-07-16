f024e94117444ce78853f1c4398f1f2145a6f037
Metin100 <9455@holbertonstudents.com>
2024-07-16 11:25:21 +0000

    sdgvrfsd

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}	
+
+	while (i < size)
+	{
+		if (cmp(array[i]))
+			{
+				return (i);
+			}
+		i++;
+	}
+	return (-1);
+}
