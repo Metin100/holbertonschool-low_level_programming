3eed3565551927c24a7ce337087cc727d6e246ec
Metin100 <9455@holbertonstudents.com>
2024-07-21 10:48:57 +0000

    dfekcvbw

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -5,23 +5,23 @@
  *
  * Return: 0 on success
  */
-
 int main(void)
 {
-	char i, j;
+	int i;
+	int j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 48; i < 57; i++)
 	{
-		for (j = i + 1; j <= '9'; j++)
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
 			{
-					putchar(i);
-					putchar(j);
-					if (i < '8')
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				putchar(',');
+				putchar(' ');
 			}
+		}
 	}
 	putchar('\n');
 	return (0);
