e8058ff0431dbd0ad2d1c514e50999c17f7f66cb
Metin100 <9455@holbertonstudents.com>
2024-07-21 10:49:35 +0000

    dfekcvbw

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -5,30 +5,29 @@
  *
  * Return: 0 on success
  */
-
 int main(void)
 {
-	char a, b, c;
+	int i;
+	int j;
+	int k;
 
-	for (a = '0'; a <= '9'; a++)
+	for (i = 48; i < 56; i++)
 	{
-		for (b = a + 1; b <= '9'; b++)
-		{	
-			for (c = b + 1; c <= '9'; c++)
+		for (j = i + 1; j < 57; j++)
+		{
+			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55 || j != 56 || k != 57)
 				{
-					putchar(a);
-					putchar(b);
-					putchar(c);
-					if (a < '7')
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
+			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
-
 }
