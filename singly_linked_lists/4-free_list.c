4df8d07956323b5643d7b63df089d413259c910a
Metin100 <9455@holbertonstudents.com>
2024-07-30 03:04:24 +0400

    Solving task

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -6,12 +6,15 @@
 
 void free_list(list_t *head)
 {
-	list_t *tmp;
-	while(head -> next != NULL)
+	list_t *current_node;
+	list_t *next_node;
+
+	current_node = head;
+	while(current_node != NULL)
 	{
-		tmp = head;
-		free(head);
-		head = tmp -> next;
+		next_node = current_node->next;
+		free(current_node->str);
+		free(current_node);
+		current_node  = next_node;
 	}
-	free(head);
 }	
