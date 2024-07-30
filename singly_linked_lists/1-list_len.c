d281b16c7196fe9cef97b0913f41b881020e4a8e
Metin100 <9455@holbertonstudents.com>
2024-07-30 10:16:57 +0000

    Fixing Betty problems

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,10 +2,16 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: header node of singly linked list
+ * Return: A number of nodes
+ */
+
 size_t list_len(const list_t *h)
 {
 	unsigned int i = 0;
-	
+
 	while (h != NULL)
 	{
 		i++;
