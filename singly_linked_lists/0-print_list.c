37a20260857209495ce29d47c053d751e7d2a2f2
Metin100 <9455@holbertonstudents.com>
2024-07-30 11:31:41 +0000

    Solving Betty Problems

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,30 +1,33 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
+
 /**
- * print_list - a function that prints all the elements of a list_t list.
- * @h: header node of singly linked list
- * Return: A number of nodes
+ * print_list - main
+ * @h: our clone of struct
+ * Return: count of node
  */
+
+
 size_t print_list(const list_t *h)
 {
-	int c = 0;
+	int i = 0;
 
-while (h != NULL)
-{
-if (h->str == NULL)
-{
-printf("[0] (nil)\n");
-			h = h->next;
-			c++;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
 		}
+
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			c++;
 		}
+		i++;
+		h = h->next;
 	}
-	return (c);
+	return (i);
+
 }
