ab55dfc41c9e88f6c808b748c36603c70027f0a0
Metin100 <9455@holbertonstudents.com>
2024-07-30 10:40:33 +0000

    Solving Betty Problems

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,49 +1,41 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "lists.h"
-
+/**
+ * add_node_end - a function that adds a new node at the end of a list_t list.
+ * @head: header node of singly linked list
+ * @str: string
+ * Return: address of the new element
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *tail, *last_node;
 	int i = 0;
+	list_t *tail;
+	list_t *last_node;
 
+	for (i = 0; str[i] != '\0'; i++)
+		;
 	tail = malloc(sizeof(list_t));
-
-	if (!tail)
-	{
-		free(tail);
+	if (tail == NULL)
 		return (NULL);
-	}
-
 	tail->str = strdup(str);
-
-	if(tail->str == NULL)
+	if (tail->str == NULL)
 	{
 		free(tail);
 		return (NULL);
 	}
-
-	while(str[i] != '\0')
-	{
-		i++;
-	}
-
 	tail->len = i;
-	tail->next = NULL;
-
-	if(*head == NULL)
+	if (*head == NULL)
 	{
 		*head = tail;
-		return(*head);
 	}
-
 	last_node = *head;
-
-	while(last_node -> next != NULL)
-			last_node = last_node -> next;
-
-	last_node -> next = tail;
-
-	return tail;
+	while (last_node->next != NULL)
+	{
+		last_node = last_node->next;
+	}
+	last_node->next = tail;
+	tail->next = NULL;
+	return (tail);
 }
