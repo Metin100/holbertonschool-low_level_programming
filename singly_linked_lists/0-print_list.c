8e0146e9b0bb5ebc60d5f8da6f328533917fcc56
Metin100 <9455@holbertonstudents.com>
2024-07-30 10:20:02 +0000

    Fixing Betty problems

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,17 +13,18 @@ size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
 
-	while (h != NULL)
+while (h != NULL)
+{
+	if (h->str == NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		} else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		h = h->next;
-		i++;
+		printf("[0] (nil)\n");
 	}
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+	}
+	h = h->next;
+	i++;
+}
 	return (i);
 }
