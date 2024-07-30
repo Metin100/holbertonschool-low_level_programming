ee361a22f5e6f6fa8bb3e0ce57f3568315305c7e
Metin100 <9455@holbertonstudents.com>
2024-07-30 12:26:42 +0000

    Fixing Betty problems

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - a function that prints all the elements of  list.
+ * @h: nodes.
+ * Return: the number of nodes.
+ *
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	unsigned int i = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
