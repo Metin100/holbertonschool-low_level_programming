93bbfe4664d55c4f43cc4579b5543cf573b09465
Metin100 <9455@holbertonstudents.com>
2024-07-30 10:42:12 +0000

    Solving Betty Problems

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -2,7 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * free_list - a function that frees a list_t list.
+ * @head: header node of singly linked list
+ * Return: free memory
+ */
 
 void free_list(list_t *head)
 {
@@ -10,11 +14,11 @@ void free_list(list_t *head)
 	list_t *next_node;
 
 	current_node = head;
-	while(current_node != NULL)
+	while (current_node != NULL)
 	{
 		next_node = current_node->next;
 		free(current_node->str);
 		free(current_node);
 		current_node  = next_node;
 	}
-}	
+}
