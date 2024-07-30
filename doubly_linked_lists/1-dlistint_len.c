f5f7f90746318cc96b7784c659fdfbb24960d001
Metin100 <9455@holbertonstudents.com>
2024-07-30 12:35:07 +0000

    Solving Betty problems

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * dlistint_len - a function returns the number of elements in a linked list.
+ * @h: nodes.
+ * Return: the number of nodes
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	int i = 0;
