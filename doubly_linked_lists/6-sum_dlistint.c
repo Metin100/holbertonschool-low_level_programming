87276e18b28df3984d0d291d39a96c5017e1f152
Metin100 <9455@holbertonstudents.com>
2024-07-30 22:28:25 +0000

    Solving Task

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stddef.h>
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
