b9ba5f2c2cba9709d08cf016a1838a441d3e1c5e
Metin100 <9455@holbertonstudents.com>
2024-07-31 15:59:04 +0400

    solving betty problems

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stddef.h>
 
+/**
+ * sum_dlistint - a function that returns the sum of all the data (n) of a list.
+ * @head: node.
+ * Return: sum of the values in node.
+ */
+
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
