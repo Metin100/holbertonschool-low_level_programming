fb52742f84ff8b685a6aede95ed51cc3fc61a5db
Metin100 <9455@holbertonstudents.com>
2024-07-31 18:25:37 +0400

    solving task

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,46 +12,43 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *prev_node;
-	dlistint_t *next_node;
-	unsigned int i = 0, len = 1;
-	
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->n = n;
-	prev_node = *h;
-	while (prev_node != NULL)
-	{
-		len++;
-		prev_node = prev_node->next;
-	}
-	prev_node = *h;
-	if (idx > len)
-	{
-		return (NULL);
-	}
-	while (i != (idx - 1) && idx != 0)
-	{
-		prev_node = prev_node->next;
-		i++;
-	}
-	next_node = prev_node->next;
-	if (prev_node == *h)
-	{
+	dlistint_t *prev_node = *h;
+	unsigned int i = 1;
+
+	new_node = NULL;
+	if (idx == 0)
 		new_node = add_dnodeint(h, n);
-		return (new_node);
-	}
-	if (next_node == NULL)
+	
+	else
 	{
-		new_node = add_dnodeint_end(h, n);
-		return (new_node);
+		while (prev_node != NULL)
+		{
+			if (i == idx)
+			{
+				if (prev_node->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node != NULL)
+					{
+						new_node->n = n;
+						new_node->prev = prev_node;
+						new_node->next = prev_node->next;
+						prev_node->next->prev = new_node;
+						prev_node->next = new_node;
+					}
+					else
+					{
+						free(new_node);
+						return (NULL);
+					}
+				}
+				break;
+			}
+			prev_node = prev_node->next;
+			i++;
+		}
 	}
-
-	new_node->prev = prev_node, prev_node->next = new_node, next_node->prev = new_node, new_node->next = next_node;
-
 	return (new_node);
 }
