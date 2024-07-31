19db993be1b50bf2c600987f4077c109c19ee655
Metin100 <9455@holbertonstudents.com>
2024-07-31 16:11:45 +0400

    solving task

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "lists.h"
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *prev_node;
+	dlistint_t *next_node;
+	unsigned int i = 0;
+	
+	new_node = malloc(sizeof(dlistint_t));
+	if(!new_node)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+	prev_node = *h;
+	if(!prev_node)
+	{
+		prev_node = new_node;
+		prev_node->next = NULL;
+		prev_node->prev = NULL;
+		return (prev_node);	
+	}
+	while(i != (idx - 1))
+	{
+		prev_node = prev_node->next;
+		i++;
+	}
+	next_node = prev_node->next;
+
+	new_node->prev = prev_node;
+	prev_node->next = new_node;
+	next_node->prev = new_node;
+	new_node->next = next_node;
+
+	return (new_node);
+}
