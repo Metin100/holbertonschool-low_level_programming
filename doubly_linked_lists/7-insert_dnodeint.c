10e7d4afffdc3e9a4d5c4b2274cf09a3f91a6b74
Metin100 <9455@holbertonstudents.com>
2024-07-31 17:06:20 +0400

    solving problem

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -47,7 +47,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (next_node == NULL)
 	{
-		return (add_dnodeint_end(&prev_node, n));
+		return (add_dnodeint_end(h, n));
 	}
 	if (i < idx)
 
