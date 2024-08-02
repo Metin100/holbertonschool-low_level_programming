54582773d064d561ac19f3084be5035cc0121aea
Metin100 <9455@holbertonstudents.com>
2024-08-02 17:04:08 +0000

    solving problemsand betty

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,49 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * binary_to_uint - function converts binary number to int.
+ * @b: input binary number.
+ * Return: the converted number.
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int len_b = 0;
 	unsigned int index = 0;
 	unsigned int c;
-	double x;
+	unsigned int x;
+	unsigned int k;
 	unsigned int result = 0;
 
-	if(b == NULL)
+	if (b == NULL)
 		return (0);
 	while (b[len_b] != '\0')
 		len_b++;
 	len_b = len_b - 1;
-
 	while (b[index] != '\0')
 	{
 		c = len_b - index;
 		if (b[index] == '0')
 		{
-			x = pow(2, c);
+			x = 1, k = 0;
+			while (k < c)
+			{
+				x = 2 * x, k++;
+			}
 			result += 0 * x;
 		}
-		else if(b[index] == '1')
+		else if (b[index] == '1')
 		{
-			x = pow(2, c);
+			x = 1, k = 0;
+			while (k < c)
+			{
+				x = 2 * x, k++;
+			}
 			result += 1 * x;
 		}
 		else
-		{
 			return (0);
-		}
 		index++;
 	}
 	return (result);
